40a8ee6ccec5f520b44ef1dee09da471ba8848a0
billsanthony <111157366+billsanthony@users.noreply.github.com>
2023-12-21 20:48:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 const express = require('express');
-const admin = require('firebase-admin');
-const serviceAccount = require("/absolute/path/to/your/serviceAccountKey.json"); // Replace with your actual path
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 // Express Middleware
 app.use(express.static('public'));
 
 // Dummy Athletes Data (Replace with MongoDB)
-const athletes = [
-    { name: 'Athlete 1', sport: 'Football', age: 25 },
-    { name: 'Athlete 2', sport: 'Basketball', age: 28 },
-    // Add more athletes
-];
+const athletes = generateRandomAthletes(50);
 
 // API Endpoint to Fetch Athletes
 app.get('/api/athletes', (req, res) => {
     res.json(athletes);
 });
+
+// Function to generate random athlete data
+function generateRandomAthletes(count) {
+    const sports = ['Football', 'Basketball', 'Tennis', 'Swimming', 'Golf'];
+    const genders = ['Male', 'Female'];
+
+    const randomAthletes = [];
+    for (let i = 1; i <= count; i++) {
+        const randomSport = sports[Math.floor(Math.random() * sports.length)];
+        const randomGender = genders[Math.floor(Math.random() * genders.length)];
+
+        const athlete = {
+            name: `Athlete ${i}`,
+            sport: randomSport,
+            gender: randomGender,
+            age: Math.floor(Math.random() * 10) + 20, // Random age between 20 and 29
+            // Add more athlete details as needed
+        };
+
+        randomAthletes.push(athlete);
+    }
+
+    return randomAthletes;
+}
 
 // Start the Server
 app.listen(PORT, () => {
